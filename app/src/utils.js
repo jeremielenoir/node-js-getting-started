dec3a84fb6584035639899cd3897c47e9535624c
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2018-01-11 00:29:10 +0100

    EVO add border and change footer apparence

--- app/src/utils.js
+++ app/src/utils.js
@@ -2,8 +2,11 @@ import request from 'request';
 import fs from 'fs';
 import gm from 'gm';
 import path from 'path';
+import mime from 'mime';
 
 import { Face, Stat } from './models';
+import config from './config';
+
 import s3bucket from './providers/aws';
 
 const addStat = async(Lang) => {
@@ -23,10 +26,10 @@ const addStat = async(Lang) => {
   }
 };
 
-const createUserFromTwitter = async(twitterUserData, number, done) => {
+/*const createUserFromTwitter = async(twitterUserData, number, done) => {
   if(twitterUserData.profile_image_url) {
     const imgDestPath = path.resolve( __dirname, '../public/img');
-    /** REFACTOR **/
+    /** REFACTOR
     try{
       const { body } = await request.get({ url: twitterUserData.profile_image_url.replace('_normal', ''), encoding: 'binary' });
       await fs.writeFile(`{$imgDestPath}/${twitterUserData.id}.jpeg`, body, 'binary');
@@ -62,6 +65,77 @@ const createUserFromTwitter = async(twitterUserData, number, done) => {
     }catch(err) {
       done(err, null);
     }
+  }
+};*/
+
+const createUserFromTwitter = function createUserFromTwitter(twitterUserData, number, done){
+    if(twitterUserData.profile_image_url){
+      const imgDestPath = path.resolve( __dirname, '../public/img');
+    /****************REFACTOR**********************/
+    request.get({url: twitterUserData.profile_image_url.replace('_normal',''), encoding: 'binary'}, function (err, response, body) {
+      fs.writeFile(imgDestPath + '/' + twitterUserData.id + '.jpeg', body, 'binary', function(errorFile) {
+        s3bucket.createBucket(function() {
+          gm(imgDestPath + '/' + twitterUserData.id + '.jpeg')
+          .resize("150", "150")
+          .stream(function(err, stdout, stderr) {
+            /***/
+            var buf = new Buffer('');
+            if(stdout){
+              stdout.on('data', function(data) {
+                 buf = Buffer.concat([buf, data]);
+              });
+              stdout.on('end', function(data) {
+                var data = {
+                  Bucket: config.S3_BUCKET_NAME,
+                  ACL: 'public-read',
+                  Key: 'img/' + twitterUserData.id + '.jpeg',
+                  Body: buf,
+                  ContentType: mime.getType(imgDestPath + '/' + twitterUserData.id + '.jpeg')
+                };
+
+
+                s3bucket.putObject(data, function(errr, res) {
+                    //console.log('CALLBACK AMAZON', errr, res);
+                    if(errr){
+                      console.log(errr);
+                    }
+                    else{
+                      var face = new Face();
+                      face.accountname = twitterUserData.name;  // set the faces name (comes from the request)
+                      face.firstname = twitterUserData.screen_name;  // set the faces name (comes from the request)
+                      face.lastname = twitterUserData.screen_name;  // set the faces name (comes from the request)
+                      face.number = number;  // set the faces name (comes from the request)
+                      face.picture = '/img/' + twitterUserData.id + '.jpeg';  // set the faces name (comes from the request)
+                      face.network = 'twitter';  // set the faces name (comes from the request)
+                      face.network_id = twitterUserData.id;  // set the faces name (comes from the request)
+                      face.lang = twitterUserData.lang;  // set the faces name (comes from the request)
+                      face.non_human = false;  // set the faces name (comes from the request)
+                      //console.log('PROFILE TWITTER', twitterUserData.id);
+
+                      //STATS
+                      addStat(face.lang);
+
+                      // save the face and check for errors
+                        face.save(function(err) {
+                            if (err){
+                              console.log(err);
+                            }
+
+                        });
+
+                        if(done){
+                          return done(null, face);
+                        }
+
+                    }
+                  });
+                });
+              }
+              /***/
+          });
+        });
+      });
+    });
   }
 };
 
